4198d3d7df603bfa5f353c44ad3f6d6a302d6b1c
Thomas Leonard <talex5@gmail.com>
2021-03-04 21:07:47 +0000

    Improve formating of bind messages

--- lib/msg.ml
+++ lib/msg.ml
@@ -150,7 +150,7 @@ let pop_and_show_arg f t : Metadata.param -> unit = function
     let interface = get_string t in
     let version = get_int t in
     let id = get_int t in
-    Fmt.pf f "(%s_%ld)%lu" interface version id
+    Fmt.pf f "+%lu(%s:v%ld)" id interface version
   | `New_ID (Some _) ->
     Fmt.pf f "+%lu" (get_int t)
   | `String ->
